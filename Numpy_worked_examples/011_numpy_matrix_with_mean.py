6f57e963ddf3ecfc1e32cd17f6291c0489591255
SADIQ PASHA <sadpasmgp@gmail.com>
2019-12-17 00:06:36 +0530

    mean in a matrix

---
+++ Numpy_worked_examples/011_numpy_matrix_with_mean.py
@@ -0,0 +1,21 @@
+"""
+Create a random vector of size 30 and find the mean value
+"""
+
+import numpy as np
+Z = np.random.random(30)
+print(Z)
+m = Z.mean()
+print(m)
+
+
+"""
+PS C:\Users\SP\Desktop\DiveintoData\Numpy> python .\011_numpy_matrix_with_mean.py             [0.50315604 0.71736089 0.3948133  0.4637216  0.36623244 0.41657264
+ 0.3418745  0.91423033 0.93268385 0.09491207 0.51644344 0.9203193
+ 0.71590616 0.73225733 0.93395274 0.36946557 0.37644213 0.70314014
+ 0.88380873 0.62322266 0.93454747 0.17668733 0.85426151 0.1788295
+ 0.98688149 0.01683071 0.07779    0.51817065 0.83445434 0.93289762]
+0.581062216945219
+PS C:\Users\SP\Desktop\DiveintoData\Numpy>  
+
+"""
