03ade8d1a42765a20076d8edc23dda5871e9c506
SADIQ PASHA <sadpasmgp@gmail.com>
2019-12-16 23:57:22 +0530

    reshape matrix

---
+++ Numpy_worked_examples/006_numpy_3X3_matrix.py
@@ -0,0 +1,15 @@
+"""
+Create a 3x3 matrix with values ranging from 0 to 8 
+"""
+
+import numpy as np
+Z = np.arange(9).reshape(3,3)
+print(Z)
+
+
+"""
+PS C:\Users\SP\Desktop\DiveintoData\Numpy> python .\006_numpy_3X3_matrix.py                   [[0 1 2]
+ [3 4 5]
+ [6 7 8]]
+PS C:\Users\SP\Desktop\DiveintoData\Numpy>                                                                                                 
+"""
