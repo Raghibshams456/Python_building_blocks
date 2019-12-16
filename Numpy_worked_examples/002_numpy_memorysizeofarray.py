8fc2f1b05def04705157db3c622fa573dc35a8ee
SADIQ PASHA <sadpasmgp@gmail.com>
2019-12-16 23:54:14 +0530

    memory size of array

---
+++ Numpy_worked_examples/002_numpy_memorysizeofarray.py
@@ -0,0 +1,25 @@
+"""
+Demonstrate finding memory size of any array
+"""
+
+import numpy as np
+nullvec = np.zeros((10,10))
+print(nullvec)
+print("%d bytes"%(nullvec.size * nullvec.itemsize))
+
+
+"""
+PS C:\Users\SP\Desktop\DiveintoData\Numpy> python .\002_numpy_memorysizeofarray.py            
+[[0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]]
+800 bytes
+PS C:\Users\SP\Desktop\DiveintoData\Numpy>   
+"""
