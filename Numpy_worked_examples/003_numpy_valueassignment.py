28f1c1326cae21c1309743c875c96ae87eca561c
SADIQ PASHA <sadpasmgp@gmail.com>
2019-12-16 23:55:06 +0530

    Changing value of entire array

---
+++ Numpy_worked_examples/003_numpy_valueassignment.py
@@ -0,0 +1,14 @@
+"""
+Create a null vector of size 10 but the fifth value which is 1
+"""
+
+import numpy as np
+nullvec = np.zeros(10)
+nullvec[4] = 1
+print(nullvec)
+
+
+"""
+PS C:\Users\SP\Desktop\DiveintoData\Numpy> python .\003_numpy_valueassignment.py              [0. 0. 0. 0. 1. 0. 0. 0. 0. 0.]
+PS C:\Users\SP\Desktop\DiveintoData\Numpy>   
+"""
