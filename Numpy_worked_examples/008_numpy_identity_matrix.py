fe5a3dbedfa14d6a17b0287b97a98e714f4b4afc
SADIQ PASHA <sadpasmgp@gmail.com>
2019-12-16 23:59:30 +0530

    identity matrix

---
+++ Numpy_worked_examples/008_numpy_identity_matrix.py
@@ -0,0 +1,14 @@
+"""
+Create a 3x3 identity matrix
+"""
+
+import numpy as np
+Z = np.eye(3)
+print(Z)
+
+"""
+PS C:\Users\SP\Desktop\DiveintoData\Numpy> python .\008_numpy_identity_matrix.py              [[1. 0. 0.]
+ [0. 1. 0.]
+ [0. 0. 1.]]
+PS C:\Users\SP\Desktop\DiveintoData\Numpy>  
+"""
